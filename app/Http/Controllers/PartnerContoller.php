35292c9b22fa102ab9cee63763edf712fc9fc4b6
assylzhan <As1kenjebaev@gmail.com>
2023-06-07 05:29:49 +0600

    qweqwe

---
+++ app/Http/Controllers/PartnerContoller.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use App\Models\Partner;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class PartnerContoller extends Controller
+{
+    public function create()
+    {
+        $partner = Auth::user();
+        return view('partner.create', ['user' => $partner]);
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name_company' => 'required|max:255',
+            'image' => 'required|mimes:png,jpg',
+        ]);
+        $imageName = "default.jpg";
+        if ($request->hasFile('image')) {
+            $imageName = $request->file('image')->getClientOriginalName();
+            $request->file('image')->storeAs('public/images/partner/photos', $imageName);
+        }
+
+
+        Auth::user()->partner()->create([
+            'name_company' => $request->input('name_company'),
+            'image' => $imageName,
+        ]);
+        return redirect()->route('gift.index')->with('message', 'Your request has been sent successfully');
+    }
+}
